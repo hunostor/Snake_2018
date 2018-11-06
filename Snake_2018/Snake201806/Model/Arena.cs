0d5de7f9ec5e2f3b3e19c228fff024526584da3a
Poroszkai Attila <poroszkai.attila@gmail.com>
2018-11-06 11:00:18 +0100

    View and Model context

---
+++ Snake_2018/Snake201806/Model/Arena.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Snake201806.Model
+{
+    /// <summary>
+    /// A játékmenet logikáját tartalmazza
+    /// </summary>
+    class Arena
+    {
+        internal void KeyDown(KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    Console.WriteLine(e.Key);
+                    break;
+                case Key.Up:
+                    break;
+                case Key.Right:
+                    break;
+                case Key.Down:
+                    break;
+            }
+        }
+    }
+}
